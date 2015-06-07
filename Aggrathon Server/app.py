fbb8df3e63247b9df7ec8c5cb44ccb19a32328ef
Aggrathon <antonbjo@gmail.com>
2015-06-07 20:33:52 +0300

    Core page (layout)

---
+++ Aggrathon Server/app.py
@@ -0,0 +1,28 @@
+"""
+This script runs the application using a development server.
+It contains the definition of routes and views for the application.
+"""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# Make the WSGI interface available at the top level so wfastcgi can get it.
+wsgi_app = app.wsgi_app
+
+
+@app.route('/')
+def main():
+    return render_template("layout.html")
+
+@app.route('/projects/')
+def projects():
+    return render_template("layout.html", title="Projects")
+
+if __name__ == '__main__':
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
